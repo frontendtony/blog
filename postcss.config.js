8a188d503e2344e7cea7be2976b046d21184ac3a
Anthony Oyathelemhi <tonero91@gmail.com>
2020-02-22 16:47:04 +0100

    fix issue with styles not bundled w/ prod build

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,4 @@
 // Copied from the official nextjs example repo for tailwindcss
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    process.env.NODE_ENV === 'production'
-      ? [
-          '@fullhuman/postcss-purgecss',
-          {
-            content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-          }
-        ]
-      : undefined,
-    'postcss-preset-env'
-  ]
+  plugins: ['tailwindcss', 'postcss-preset-env']
 };
