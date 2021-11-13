8703df87d6b8903052b618e903a55db7b8049555
Anthony Oyathelemhi <tonero91@gmail.com>
2021-11-13 15:13:41 +0100

    second attempt at fixing opengraph issues

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import { DefaultSeo } from 'next-seo';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import Layout from '../components/layout';
@@ -7,51 +6,21 @@ import '../styles/index.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <DefaultSeo
-        openGraph={{
-          type: 'website',
-          site_name: 'Anthony Oyathelemhi',
-          title: 'Anthony Oyathelemhi - Software Developer',
-          description: 'Frontend Developer, Typescript and all things React',
-          profile: {
-            firstName: 'Anthony',
-            lastName: 'Oyathelemhi',
-            gender: 'male',
-          },
-          images: [
-            {
-              url: '/images/opengraph.jpg',
-              alt: 'Anthony Oyathelemhi, Software Engineer',
-              width: 1200,
-              height: 627,
-            },
-          ],
-          locale: 'en_US',
-          url: 'https://frontendtony.com/',
-        }}
-        twitter={{
-          handle: '@frontendtony',
-          site: '@frontendtony',
-          cardType: 'summary_large_image',
-        }}
-      />
-      <ThemeProvider>
-        <Head>
-          <title>Anthony Oyathelemhi - Software Developer</title>
-          <meta name="description" content="Frontend Developer, Typescript and all things React" />
+    <ThemeProvider>
+      <Head>
+        <title>Anthony Oyathelemhi - Software Developer</title>
+        <meta name="description" content="Frontend Developer, Typescript and all things React" />
 
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
-          <meta name="theme-color" content="#ffffff"></meta>
-        </Head>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeProvider>
-    </>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+        <meta name="theme-color" content="#ffffff"></meta>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { promises as fs } from 'fs';
 import matter from 'gray-matter';
+import { NextSeo } from 'next-seo';
 import Image from 'next/image';
 import Link from 'next/link';
 import path from 'path';
@@ -22,65 +23,95 @@ type Blog = {
 
 const Home = ({ allBlogs }: { allBlogs: Blog[] }) => {
   return (
-    <main className="container mx-auto p-2 md:px-8">
-      <div className="absolute right-0 mr-4 mt-2 md:mr-6">
-        <ThemeToggle />
-      </div>
-      <div className="flex flex-col items-center self-center px-4 mt-20 md:mt-28 mx-auto mb-8 max-w-md">
-        <Image
-          src={avatarUrl}
-          alt="Avatar of Anthony Oyathelemhi"
-          className="rounded-full mr-4"
-          width={160}
-          height={160}
-        />
-        <p className="text-center text-3xl font-bold leading-none mt-4">Anthony Oyathelemhi</p>
-        <p className="text-center text-lg my-2 opacity-75">
-          Sofware Engineer, Master of all thing Frontend
-        </p>
-        <div className="flex items-center mt-4">
-          <a
-            href="http://twitter.com/frontendtony"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mx-3"
-          >
-            <Image src={twitterImageUrl} alt="Twitter logo" />
-          </a>
-          <a
-            href="http://github.com/frontendtony"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mx-3"
-          >
-            <Image src={githubImageUrl} alt="Github logo" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/tonerolima/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mx-3"
-          >
-            <Image src={linkedInImageUrl} alt="Linkedin logo" />
-          </a>
+    <>
+      <NextSeo
+        openGraph={{
+          type: 'website',
+          site_name: 'Anthony Oyathelemhi',
+          title: 'Anthony Oyathelemhi - Software Developer',
+          description: 'Frontend Developer, Typescript and all things React',
+          profile: {
+            firstName: 'Anthony',
+            lastName: 'Oyathelemhi',
+            gender: 'male',
+          },
+          images: [
+            {
+              url: '/images/opengraph.jpg',
+              alt: 'Anthony Oyathelemhi, Software Engineer',
+              width: 1200,
+              height: 627,
+            },
+          ],
+          locale: 'en_US',
+          url: 'https://frontendtony.com/',
+        }}
+        twitter={{
+          handle: '@frontendtony',
+          site: '@frontendtony',
+          cardType: 'summary_large_image',
+        }}
+      />
+      <main className="container mx-auto p-2 md:px-8">
+        <div className="absolute right-0 mr-4 mt-2 md:mr-6">
+          <ThemeToggle />
         </div>
-      </div>
-      <ul className="max-w-3xl mx-auto list-none p-0">
-        {allBlogs.map(blog => (
-          <li className="mb-4 md:mb-12 p-6 md:p-12 bg-secondary shadow-md" key={blog.title}>
-            <Link href="/[post]" as={`/${blog.slug}`}>
-              <a className="no-underline">
-                <h1 className="m-0 mb-2 text-2xl md:text-3xl">{blog.title}</h1>
-                <p className="mb-0 md:mb-8 mt-0 text-sm text-primary">
-                  {formatDate(blog.date)} - {blog.estimatedReadingTime}
-                </p>
-                <p className="text-md">{blog.summary}</p>
-              </a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </main>
+        <div className="flex flex-col items-center self-center px-4 mt-20 md:mt-28 mx-auto mb-8 max-w-md">
+          <Image
+            src={avatarUrl}
+            alt="Avatar of Anthony Oyathelemhi"
+            className="rounded-full mr-4"
+            width={160}
+            height={160}
+          />
+          <p className="text-center text-3xl font-bold leading-none mt-4">Anthony Oyathelemhi</p>
+          <p className="text-center text-lg my-2 opacity-75">
+            Sofware Engineer, Master of all thing Frontend
+          </p>
+          <div className="flex items-center mt-4">
+            <a
+              href="http://twitter.com/frontendtony"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mx-3"
+            >
+              <Image src={twitterImageUrl} alt="Twitter logo" />
+            </a>
+            <a
+              href="http://github.com/frontendtony"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mx-3"
+            >
+              <Image src={githubImageUrl} alt="Github logo" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/tonerolima/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mx-3"
+            >
+              <Image src={linkedInImageUrl} alt="Linkedin logo" />
+            </a>
+          </div>
+        </div>
+        <ul className="max-w-3xl mx-auto list-none p-0">
+          {allBlogs.map(blog => (
+            <li className="mb-4 md:mb-12 p-6 md:p-12 bg-secondary shadow-md" key={blog.title}>
+              <Link href="/[post]" as={`/${blog.slug}`}>
+                <a className="no-underline">
+                  <h1 className="m-0 mb-2 text-2xl md:text-3xl">{blog.title}</h1>
+                  <p className="mb-0 md:mb-8 mt-0 text-sm text-primary">
+                    {formatDate(blog.date)} - {blog.estimatedReadingTime}
+                  </p>
+                  <p className="text-md">{blog.summary}</p>
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
   );
 };
 
