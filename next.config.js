015b319115c341819c779f416feec045de8c12ca
Anthony Oyathelemhi <tonero91@gmail.com>
2021-11-13 09:08:25 +0100

    improve accessibility

--- next.config.js
+++ next.config.js
@@ -2,9 +2,13 @@ module.exports = {
   webpack: config => {
     config.module.rules.push({
       test: /\.md$/,
-      use: 'raw-loader'
+      use: 'raw-loader',
     });
 
     return config;
-  }
+  },
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
 };
