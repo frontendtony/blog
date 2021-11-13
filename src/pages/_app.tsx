712774f677e1e9def99105c9fbd638b944e3d5a0
Anthony Oyathelemhi <tonero91@gmail.com>
2021-11-13 09:10:30 +0100

    improve SEO - add site description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -39,8 +39,9 @@ function MyApp({ Component, pageProps }: AppProps) {
       <ThemeProvider>
         <Head>
           <title>Anthony Oyathelemhi - Software Developer</title>
-          <link rel="icon" href="/favicon.ico" />
+          <meta name="description" content="Frontend Developer, Typescript and all things React" />
 
+          <link rel="icon" href="/favicon.ico" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
