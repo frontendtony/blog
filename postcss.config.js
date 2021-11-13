b25e9caf5d26e04dd2459bd13bf903bc9f6a6430
Anthony Oyathelemhi <tonero91@gmail.com>
2021-11-13 08:58:04 +0100

    fix tailwindcss build issue

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,6 @@
-// Copied from the official nextjs example repo for tailwindcss
 module.exports = {
-  plugins: ['tailwindcss', 'postcss-preset-env']
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const plugin = require('tailwindcss/plugin');
 
 module.exports = {
   mode: 'jit',
-  purge: ['./src/pages/**/*.[tsx]', './src/components/**/*.[tsx]'],
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       spacing: {
